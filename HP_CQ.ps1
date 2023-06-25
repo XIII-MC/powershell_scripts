a91c02b7e7b76844bf805344f9d2f823509d28d0
XIII___ <81795405+XIII-MC@users.noreply.github.com>
2023-06-25 09:49:56 +0200

    Added file
    Some script i use in an old game that keeps a key pressed so i can do combos easily lol

---
+++ HP_CQ.ps1
@@ -0,0 +1,20 @@
+﻿Add-Type @"
+using System;                                                                     
+using System.Runtime.InteropServices;
+public class Tricks {
+[DllImport("user32.dll")]                                                            
+public static extern void keybd_event(byte bVk, byte bScan, uint dwFlags, int dwExtraInfo);}
+"@   
+
+[system.reflection.assembly]::Loadwithpartialname("system.windows.forms")
+
+#left shift key pressed
+[tricks]::keybd_event([System.Windows.Forms.Keys]::K, 0x45, 0, 0);
+
+WHILE ($True) {
+    [tricks]::keybd_event([System.Windows.Forms.Keys]::K, 0x45, 0, 0);
+    sleep -Seconds 3
+}
+
+#left shift key released
+#[tricks]::keybd_event([System.Windows.Forms.Keys]::LShiftKey, 0x45, 0x2, 0); 
